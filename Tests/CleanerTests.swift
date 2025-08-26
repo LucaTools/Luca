521d8b360e28884a36efe1a4a364f851371bd213
Alberto De Bortoli <albertodebortoli@gmail.com>
2025-08-26 22:22:23 +0100

    Implement basic unit tests

---
+++ Tests/CleanerTests.swift
@@ -0,0 +1,28 @@
+//  CleanerTests.swift
+
+import XCTest
+@testable import LucaCore
+
+class CleanerTests: XCTestCase {
+
+    func test_clean() throws {
+        let fileManager = MyFileManager()
+
+        let toolsFolder = fileManager.homeDirectoryForCurrentUser
+            .appending(components: Constants.toolFolder, Constants.toolsFolder)
+        try fileManager.createDirectory(at: toolsFolder, withIntermediateDirectories: true)
+
+        let activeFolder = URL(string: fileManager.currentDirectoryPath)!
+            .appending(components: Constants.toolFolder, Constants.activeFolder)
+        try fileManager.createDirectory(at: activeFolder, withIntermediateDirectories: true)
+
+        XCTAssertTrue(fileManager.fileExists(atPath: toolsFolder.path))
+        XCTAssertTrue(fileManager.fileExists(atPath: activeFolder.path))
+
+        let cleaner = Cleaner(fileManager: fileManager)
+        try cleaner.clean()
+
+        XCTAssertFalse(fileManager.fileExists(atPath: toolsFolder.path))
+        XCTAssertFalse(fileManager.fileExists(atPath: activeFolder.path))
+    }
+}
