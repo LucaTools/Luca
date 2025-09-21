f660cbc27386814246a04a3db4d2961835514201
Alberto De Bortoli <albertodebortoli@gmail.com>
2025-09-21 01:56:25 +0100

    Implement FileManagerWrapper

---
+++ Sources/LucaCLI/Utils/FileManagerWrapper.swift
@@ -0,0 +1,77 @@
+//  FileManagerWrapper.swift
+
+import Foundation
+import LucaCore
+
+public struct FileManagerWrapper: FileManaging {
+    
+    private(set) var fileManager: FileManager
+    
+    init(fileManager: FileManager = .default) {
+        self.fileManager = fileManager
+    }
+    
+    /// Root folder for all installed versions.
+    public var toolsFolder: URL {
+        homeDirectoryForCurrentUser
+            .appending(components: Constants.toolFolder, Constants.toolsFolder)
+    }
+
+    /// Folder containing symlinks to currently active binaries (per working directory).
+    public var activeFolder: URL {
+        URL(fileURLWithPath: currentDirectoryPath)
+            .appending(components: Constants.toolFolder, Constants.activeFolder)
+    }
+    
+    public var homeDirectoryForCurrentUser: URL {
+        fileManager.homeDirectoryForCurrentUser
+    }
+
+    public var currentDirectoryPath: String {
+        fileManager.currentDirectoryPath
+    }
+    
+    public func fileExists(atPath path: String) -> Bool {
+        fileManager.fileExists(atPath: path)
+    }
+    
+    public func removeItem(at path: URL) throws {
+        try fileManager.removeItem(at: path)
+    }
+    
+    public func removeItem(atPath path: String) throws {
+        try fileManager.removeItem(atPath: path)
+    }
+    
+    public func moveItem(at: URL, to: URL) throws {
+        try fileManager.moveItem(at: at, to: to)
+    }
+    
+    public func createDirectory(at url: URL, withIntermediateDirectories: Bool) throws {
+        try fileManager.createDirectory(at: url, withIntermediateDirectories: withIntermediateDirectories)
+    }
+    
+    public func createSymbolicLink(at url: URL, withDestinationURL destinationURL: URL) throws {
+        try fileManager.createSymbolicLink(at: url, withDestinationURL: destinationURL)
+    }
+    
+    public func destinationOfSymbolicLink(atPath path: String) throws -> String {
+        try fileManager.destinationOfSymbolicLink(atPath: path)
+    }
+    
+    public func enumerator(at url: URL, includingPropertiesForKeys keys: [URLResourceKey]?, options mask: FileManager.DirectoryEnumerationOptions) -> FileManager.DirectoryEnumerator? {
+        fileManager.enumerator(at: url, includingPropertiesForKeys: keys, options: mask)
+    }
+    
+    public func setAttributes(_ attributes: [FileAttributeKey: Any], ofItemAtPath path: String) throws {
+        try fileManager.setAttributes(attributes, ofItemAtPath: path)
+    }
+    
+    public func contentsOfDirectory(at url: URL, includingPropertiesForKeys keys: [URLResourceKey]?, options mask: FileManager.DirectoryEnumerationOptions) throws -> [URL] {
+        try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: keys, options: mask)
+    }
+    
+    public func attributesOfItem(atPath path: String) throws -> [FileAttributeKey: Any] {
+        try fileManager.attributesOfItem(atPath: path)
+    }
+}
