add9d6d229e029e8736dccd38f054a1bac6b90d3
Alberto De Bortoli <albertodebortoli@gmail.com>
2025-09-21 01:57:13 +0100

    Implement LucaCommand

---
+++ Sources/LucaCLI/Commands/LucaCommand.swift
@@ -0,0 +1,17 @@
+//  LucaCommand.swift
+
+import ArgumentParser
+import Foundation
+
+@main
+struct LucaCommand: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "luca",
+        abstract: "A modern tool manager that helps you install and manage development tools.",
+        version: version,
+        subcommands: [
+            InstallCommand.self,
+            UninstallCommand.self
+        ]
+    )
+}
