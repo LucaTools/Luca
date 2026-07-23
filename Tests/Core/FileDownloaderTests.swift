87c899f944450eae2da0851a650337ec853d8738
Alberto De Bortoli <albertodebortoli@gmail.com>
2026-07-23 09:15:17 +0100

    refactor(download): make FileDownloading request-based

--- Tests/Core/FileDownloaderTests.swift
+++ Tests/Core/FileDownloaderTests.swift
@@ -26,7 +26,7 @@ struct FileDownloaderTests {
         let sut = FileDownloader(session: URLSession(configuration: config))
 
         let url = try #require(URL(string: "https://example.com/tool.zip"))
-        let (downloadedURL, response) = try await sut.download(from: url)
+        let (downloadedURL, response) = try await sut.download(for: URLRequest(url: url))
 
         #expect(downloadedURL.isFileURL)
         let httpResponse = try #require(response as? HTTPURLResponse)
@@ -42,8 +42,28 @@ struct FileDownloaderTests {
 
         let url = try #require(URL(string: "https://example.com/tool.zip"))
         await #expect(throws: (any Error).self) {
-            _ = try await sut.download(from: url)
+            _ = try await sut.download(for: URLRequest(url: url))
         }
+    }
+
+    @Test
+    func test_download_forwardsRequestHeaders() async throws {
+        var capturedRequest: URLRequest?
+        MockFileURLProtocol.requestHandler = { request in
+            capturedRequest = request
+            let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            return (response, Data("file contents".utf8))
+        }
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockFileURLProtocol.self]
+        let sut = FileDownloader(session: URLSession(configuration: config))
+
+        let url = try #require(URL(string: "https://example.com/tool.zip"))
+        var request = URLRequest(url: url)
+        request.setValue("Bearer test-token-value", forHTTPHeaderField: "Authorization")
+        _ = try await sut.download(for: request)
+
+        #expect(capturedRequest?.value(forHTTPHeaderField: "Authorization") == "Bearer test-token-value")
     }
 }
 
